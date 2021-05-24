2e999f412a00761f806119a3cf222dd606a26ab6
andruha <molodoyelpil>
2021-05-24 10:44:04 +0300

    commit name

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
